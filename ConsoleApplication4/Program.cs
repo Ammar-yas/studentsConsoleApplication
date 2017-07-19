a2a3bfe034f4ee398abcb2703bc7984d3725e498
Ammar <plus.ammaryasser@gmail.vom>
2017-07-19 21:43:50 +0200

    Add project files.

---
+++ ConsoleApplication4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students student = new Students();
+            string StudentDegree;
+            Console.WriteLine("Please input the student id");
+            int studentNumber = int.Parse(Console.ReadLine());
+            StudentDegree = student.GetStudentDegree(studentNumber);
+            Console.WriteLine("Degree of student with number " + studentNumber
+                + " is :\n" + StudentDegree);
+            
+        }
+    }
+}
---
+++ ConsoleApplication4/Students.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Students
+    {
+        public string GetStudentDegree (int studentNumber)
+        {
+            string studentDegree;
+            switch (studentNumber)
+            {
+                case 1:
+                    studentDegree = "Software = 23, DSS = 20, OR = 30, math = 25";
+                    break;
+                case 2:
+                    studentDegree = "software = 21, DSS = 25, OR = 28,  math = 3";
+                    break;
+                case 3:
+                    studentDegree = "software = 19, DSS = 30, OR = 29, math = 20";
+                    break;
+                default:
+                    studentDegree = "Wrong student number";
+                    break;
+            }
+            return studentDegree;
+        }
+    }
+}
